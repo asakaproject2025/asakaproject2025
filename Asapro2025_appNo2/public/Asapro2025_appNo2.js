5db64e78473cf369542458511b3de680d33dc808
asakaproject2025 <asakaproject2025@gmail.com>
2025-11-28 06:40:27 +0900

    Update Asapro2025_appNo2.js

--- Asapro2025_appNo2/public/Asapro2025_appNo2.js
+++ Asapro2025_appNo2/public/Asapro2025_appNo2.js
@@ -564,11 +564,11 @@ document.addEventListener("DOMContentLoaded", async function () {
                                         groupedByFloor[floor].forEach(room => {
                                                 const btn = document.createElement("button");
                                                 // statusに基づいて色を決定
-                                                btn.className = `room ${room.status === 0 ? "blue" : "red"}`;
+                                                btn.className = `room ${room.status === "0" ? "blue" : "red"}`;
                                                 //色覚多様性の時のみ丸バツを表記
                                                 let roomName = room.name;
                                                 if (currentTheme === 'colorblind') {
-                                                        const symbol = room.status === 0 ? '⚪︎' : '✕';
+                                                        const symbol = room.status === "0" ? '⚪︎' : '✕';
                                                         roomName = symbol + ' ' + roomName;
                                                 }
                                                 
